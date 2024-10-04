4782861210c3e965059f59452e506396a536f5da
JuanMa Garrido <juanma.garrido@gmail.com>
2024-10-04 11:33:14 +0100

    updates starter behaviour

--- src/view.js
+++ src/view.js
@@ -1,33 +1,22 @@
-import { store } from '@wordpress/interactivity';
+import { store } from "@wordpress/interactivity";
 
-const { state } = store( 'router-2f43f8', {
-	state: {
-		urlPathname: '',
-		urlRegionDisplay: window.location.href,
-		get urlRegionDisplaySlug() {
-			const isHome = state.urlPathname === '/';
-			return isHome
-				? '/'
-				: '/' + state.urlPathname.split( '/' ).filter( Boolean ).pop();
-		},
-		get isPlayground() {
-			return state.urlPathname.includes( 'scope' );
-		},
-	},
-	callbacks: {
-		setUrlPathname: () => {
-			state.urlPathname = new URL( state.urlRegionDisplay ).pathname;
-		},
-	},
-	actions: {
-		*navigate( e ) {
-			debugger;
-			e.preventDefault();
-			const { actions } = yield import(
-				'@wordpress/interactivity-router'
-			);
-			state.urlRegionDisplay = e.target.href;
-			yield actions.navigate( state.urlRegionDisplaySlug );
-		},
-	},
-} );
+const { state } = store("router-2f43f8", {
+  state: {
+    urlRegionDisplay: window.location.href,
+    get urlRegionDisplaySlug() {
+      const { pathname } = new URL(state.urlRegionDisplay);
+      const isHome = pathname === "/";
+      return isHome ? "/" : "/" + pathname.split("/").filter(Boolean).pop();
+    },
+  },
+  actions: {
+    *navigate(e) {
+      e.preventDefault();
+      const { actions } = yield import("@wordpress/interactivity-router");
+      state.urlRegionDisplay = e.target.href;
+
+      //yield actions.navigate( state.urlRegionDisplaySlug );
+      yield actions.navigate(e.target.href);
+    },
+  },
+});
